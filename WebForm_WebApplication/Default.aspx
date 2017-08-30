4ee2a2d02613dc577fc3e895c0f4360a0cb9783d
Pap Sogome <ssogome@gmail.com>
2017-08-29 21:23:10 -0600

    Add project files.

---
+++ WebForm_WebApplication/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebForm_WebApplication._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+      <br /><br /><br /><br />
+
+        <div>           
+                <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="true" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">                    
+                </asp:DropDownList>                         
+                <asp:Label ID="Label2" ForeColor="Red" runat="server" Text="Your selection"></asp:Label>
+        </div>
+    <br /><br />
+        <div>           
+            <asp:TextBox ID="TextBox1" runat="server">Enter Integer#1</asp:TextBox>
+            <asp:TextBox ID="TextBox2" runat="server">Enter Integer#2</asp:TextBox>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="SubstractButton" />
+            <asp:Label ID="Label1" ForeColor="Red"  runat="server" Text="SubstractResult"></asp:Label>
+           
+        </div>       
+
+</asp:Content>
---
+++ WebForm_WebApplication/Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebForm_WebApplication
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          
+            if (!Page.IsPostBack)
+            {
+                List<string> lst = new List<string>()
+                 {
+                         "Item 1 from list",
+                         "Item 2 from list"
+                 };
+                DropDownList1.DataSource = lst;
+                DropDownList1.DataBind();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Label1.Text = (Convert.ToInt32(TextBox1.Text) -Convert.ToInt32(TextBox2.Text)).ToString();
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (DropDownList1.SelectedIndex)
+            {
+                case 0:
+                    Label2.Text = "You have selected " + "\" " + DropDownList1.Text + "\"";
+                    break;
+                case 1:
+                    Label2.Text = "Your selected option is: " + "\"" + DropDownList1.Text + "\"";
+                    break; 
+            }
+        }
+    }
+}
